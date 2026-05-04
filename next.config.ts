31876ca21b9df2cb1b1aeee3433d3a4f8cfb4452
Bulent Erturk <bulent.erturk@parisgayrimenkul.com>
2026-05-04 21:18:47 +0300

    Remove basePath, add CNAME for zekahub.com

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/zekahub-website',
-  assetPrefix: '/zekahub-website',
   images: {
     unoptimized: true,
   },
